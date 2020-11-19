db4831ea8b64ff71c6bb8a8d359830c98661cb7a
Nick Timms <nicktimms1@hotmail.com>
2020-11-19 15:37:36 -0700

    Attempted TowerHanoi but need work

---
+++ towerOfHanoi.js
@@ -0,0 +1,13 @@
+// There are three towers.The objective of the game is to move all the disks over to tower #3, 
+// but you can't place a larger disk onto a smaller disk. To play the game or learn more about the Tower of Hanoi, check the Resources tab.
+
+// Create a function that takes a number discs as an argument and returns the minimum amount of steps needed to complete the game.
+// towerHanoi(3) ➞ 7
+// towerHanoi(5) ➞ 31
+// towerHanoi(0) ➞ 0
+
+
+// Input: a Number representitive of the number of disks on the first poll
+// Output: the number of moves neccessary for completion
+
+// NO IDEA
