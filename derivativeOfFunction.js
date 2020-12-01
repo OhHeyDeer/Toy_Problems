5693c148d30541d1a972359f429eae77d2084f67
Nick Timms <nicktimms1@hotmail.com>
2020-12-01 12:52:26 -0700

    Finished Derivative of a Function Toy Problem

---
+++ derivativeOfFunction.js
@@ -0,0 +1,34 @@
+// Create a function that takes numbers b and m as arguments 
+// and returns the derivative of the function f(x)=x ^ b with respect to x evaluated at x = m, where b and m are constants.
+
+// derivative(1, 4) ➞ 1
+// --- 4^0 * 1
+// ---- 1
+// derivative(3, -2) ➞ 12
+// --- -2 ^ 3
+// --- -2^2 * 3
+// ---- 12
+// derivative(4, -3) ➞ -108
+// --- -3^4
+// --- -3^3 * 4
+// ---- -108
+
+
+
+// Inputs: two number values -- b,m
+// Output: the derivative of the two numbers
+
+
+
+const derivative = (b, m) => {
+
+    // Raise the m value to the b - 1th power 
+    // Multiply the equation by the b value
+    const formula = (m**(b-1)) * b;
+    // return value
+    return formula;
+}
+
+console.log(derivative(1, 4));
+console.log(derivative(3, -2));
+console.log(derivative(4, -3));
