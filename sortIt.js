87e35b28fcd8460a05ebdfe7f445277f302c4b46
Nick Timms <nicktimms1@hotmail.com>
2020-12-01 13:28:17 -0700

    Working on sortIt Problem, have yet to find a solution

---
+++ sortIt.js
@@ -0,0 +1,32 @@
+// Create a function that, given an array similar to the above, sorts the array according to the "content of the elements".
+// To reiterate, elements of the array will be either integers or arrays with a single integer.
+
+
+// sortIt([4, 1, 3]) ➞ [1, 3, 4]
+// sortIt([[4], [1], [3]]) ➞[[1], [3], [4]]
+// sortIt([4, [1], 3]) ➞[[1], 3, 4]
+// sortIt([[4], 1, [3]]) ➞[1, [3], [4]]
+// sortIt([[3], 4, [2], [5], 1, 6]) ➞[1, [2], [3], 4, [5], 6]
+
+
+// Input: An array of values and arrays with single values
+// Output: the array sorted but not mutated
+
+
+
+
+const sortIt = (weirdArray) => {
+
+    // 
+    
+
+}
+
+
+
+console.log(sortIt([4, 1, 3]));
+console.log(sortIt([[4], [1], [3]]));
+console.log(sortIt([4, [1], 3]));
+console.log(sortIt([[4], 1, [3]]));
+console.log(sortIt([[3], 4, [2], [5], 1, 6]));
+
