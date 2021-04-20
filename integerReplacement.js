05a23542178118635b4b3b6c942fe4540a2fd1fd
Nick Timms <nicktimms1@hotmail.com>
2021-04-20 10:31:01 -0600

    IntegerReplacement Toy Problem Complete

--- integerReplacement.js
+++ integerReplacement.js
@@ -7,28 +7,29 @@
 // Return the minimum number of operations to get the number to 1
 
 const integerReplacement = (n) => {
-
     // While n is not equal to 1, loop through and count the number of operations
     let count = 0;
-    let current = n;
+    let current = n; // Redefine n as a holding variable
     while (current !== 1) {
 
         if (current % 2 === 0) { // If Even
             current = current/2;
-        } else {
-            if (current < 1) { // if the current is below and needs to increase
-
-            } else { // if the current is larger than 1 needs to decrease
-
+        } else { // If Odd
+            
+            if (current < 0) { // If the current is below and needs to decrease
+                current = current - 1;
+            } else { // If the current is larger than 1 needs to increase
+                current = current + 1;
             }
         }
-        count++;
+        count++; // Increase the operation count
     }
-
-
-
+    return count;
 };
 
+
+// Test Cases -- All Pass
 console.log(integerReplacement(8)); // 3
 console.log(integerReplacement(7)); // 4
 console.log(integerReplacement(4)); // 2 
+
