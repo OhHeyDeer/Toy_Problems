bbb85897e0a84baafcb1a44b5a48f38e97241a45
Nick Timms <nicktimms1@hotmail.com>
2021-04-22 11:15:34 -0600

    Setup for containsDuplicates Toy Problem/

---
+++ containsDuplicate.js
@@ -0,0 +1,20 @@
+// Given an integer array nums
+// return true if any value appears at least twice in the array
+// return false if every element is distinct
+
+
+const containsDuplicate = (arrayOfInts) => {
+
+
+
+
+
+}
+
+
+
+// Test Cases
+console.log(containsDuplicate([1, 2, 3, 1])); // True 
+console.log(containsDuplicate([1, 2, 3, 4])); // False
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
+
